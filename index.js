e213aa87353f77eb476de4ae39467f32266d32ec
skmutai6 <skmutai6@gmail.com>
2024-03-20 07:06:19 +0300

    add strings

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Shades';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${SHADES} !";
+
+const shortGreeting = 'Welcome, S !';
